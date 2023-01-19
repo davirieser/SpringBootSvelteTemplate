340a1daee5d19c65ef7c4f8e365e473254582104
davirieser <david.w.rieser@gmail.com>
2023-01-19 15:47:12 +0100

    Added Frontend Error Page

---
+++ frontend/src/error/index.ts
@@ -0,0 +1,11 @@
+import "../app.postcss";
+import App from "./Error.svelte";
+
+// import '@brainandbones/skeleton/themes/theme-skeleton.css';
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
+
